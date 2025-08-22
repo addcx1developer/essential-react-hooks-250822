e39f2b43a06f06ec6dd3ad2c80208d5c6a0b16e0
ALEXIS D. DELA CRUZ <addcx1developer@gmail.com>
2025-08-22 20:42:38 +0800

    each component have own private state

--- src/components/ReactState.jsx
+++ src/components/ReactState.jsx
@@ -5,6 +5,7 @@ export default function ReactState() {
     <div className="min-h-screen bg-gradient-to-b from-gray-800 to-gray-900 text-white flex flex-col items-center justify-center p-8">
       <h1 className="text-5xl font-bold mb-12">How Does State Really Work In React.js?</h1>
       <CountComponent />
+      <CountComponent />
     </div>
   )
 }
